1ba638ffdb21a08135d3c76fafe719733aa5ffea
Bohun Kim <hoppang@gmail.com>
2022-08-06 00:21:02 +0900

    fix lint

--- src/renderer_viewer.ts
+++ src/renderer_viewer.ts
@@ -1,6 +1,6 @@
 import { ipcRenderer } from 'electron';
 
 ipcRenderer.on('load_image', (_event, fileName) => {
-    var canvas = document.getElementById('canvas') as HTMLImageElement;
+    const canvas = document.getElementById('canvas') as HTMLImageElement;
     canvas.src = fileName;
 });
