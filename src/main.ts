c2905c6c0aab459a9f8d134e065a21dd3387d727
Bohun Kim <hoppang@gmail.com>
2022-08-15 00:24:43 +0900

    #30 MainForm 파일 분리 + 불필요 로그 삭제

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,15 @@ import IViewer from './viewer/iviewer';
 import ImageViewer from './viewer/image_viewer';
 import CBZViewer from './viewer/cbz_viewer';
 import SettingsManager from './managers/settings_manager';
+import MainForm from './mainform';
 
-let main: Main;
+let main: MainForm;
 let viewer: IViewer;
 let browser: Browser;
 
 app.whenReady().then(async () => {
     log.info('arguments: ' + process.argv);
-    main = new Main();
-
+    main = new MainForm();
     browser = new Browser(main.win());
 
     if (process.argv.length == 3) {
@@ -36,38 +36,13 @@ app.whenReady().then(async () => {
     }
 });
 
-class Main {
-    private _win: BrowserWindow;
-
-    constructor() {
-        this._win = new BrowserWindow({
-            width: 1024,
-            height: 768,
-            minWidth: 800,
-            minHeight: 600,
-            // hack for 'require is not defined'
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false,
-            },
-        });
-    }
-
-    win(): BrowserWindow {
-        return this._win;
-    }
-}
-
 ipcMain.on('cd', (_event, dirname: string) => {
     log.info('cd to ' + dirname);
     browser.chdir(dirname);
 });
 
 ipcMain.on('view', (_event, parameter: { cwd: string; filename: string }) => {
-    log.info(util.format('view [url] %s [filename] %s', parameter.cwd, parameter.filename));
-
     const fileType = Util.getFileType(parameter.filename);
-    log.info('fileType: ' + fileType);
 
     switch (fileType) {
         case FILE_TYPE.CBZ:
