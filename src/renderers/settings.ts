f6283eba249ca8c0576003c2e44a12762177e4de
Bohun Kim <hoppang@gmail.com>
2022-09-12 22:03:35 +0900

    #46 캐시 디렉토리 크기 표시를 보기 편하게

--- src/renderers/settings.ts
+++ src/renderers/settings.ts
@@ -37,7 +37,7 @@ ipcRenderer.on('response_settings_ready', (event, params) => {
     rememberLastDir.checked = params.rememberLastDir;
 
     const cacheInfo = document.getElementById('cache_info') as HTMLDivElement;
-    cacheInfo.innerHTML = util.format('cache size: [%d]', params.tempDirSize);
+    cacheInfo.innerHTML = util.format('cache size: [%s]', humanFileSize(params.tempDirSize));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -58,4 +58,36 @@ function clearCache() {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function onClickRemote() {
     ipcRenderer.send('load_remote_page');
+}
+
+/**
+ * Format bytes as human-readable text.
+ * from https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string
+ *
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ *
+ * @return Formatted string.
+ */
+function humanFileSize(bytes: number, si = false, dp = 1) {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+
+    const units = si
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10 ** dp;
+
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+    return bytes.toFixed(dp) + ' ' + units[u];
 }
